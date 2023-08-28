cb95f3056e1f0abec0076d5a634b5b50affcea15
PRPA1984 <m.p.a@live.com.ar>
2023-08-28 13:19:23 -0300

    Relacion unidireccional con Domicilio

--- src/main/java/com/utn/ejercicio1/Ejercicio1Application.java
+++ src/main/java/com/utn/ejercicio1/Ejercicio1Application.java
@@ -1,5 +1,6 @@
 package com.utn.ejercicio1;
 
+import com.utn.ejercicio1.entidades.Domicilio;
 import com.utn.ejercicio1.entidades.Persona;
 import com.utn.ejercicio1.repositorios.PersonaRepository;
 import jakarta.annotation.PostConstruct;
@@ -24,32 +25,37 @@ public class Ejercicio1Application {
 
 	}
 
-
-
 	@Bean
-	CommandLineRunner init(PersonaRepository personaRepo) {
-		return args -> {
-			System.out.println("-----------------ESTOY FUNCIONANDO---------");
-			Persona persona = new Persona();
-			persona.setNombre("Juan");
-			persona.setApellido("Pérez");
-			persona.setEdad(30);
+	CommandLineRunner init(PersonaRepository personaRepo) { return args -> {
+		System.out.println("-----------------ESTOY FUNCIONANDO---------"); /*El método builder() se genera automáticamente por Lombok
+y te permite crear una instancia de Persona.Builder. Luego, puedes encadenar llamadas a los métodos setters generados automáticamente para establecer los valores de los atributos de la clase.
+Finalmente, build() crea la instancia
+ de la clase Persona con los valores proporcionados.
+*/
+		Persona persona = Persona.builder()
+				.nombre("Juan")
+				.apellido("Pérez")
+				.edad(30)
+				.build();
+		Domicilio domicilio = Domicilio.builder()
+				.calle("Suipacha")
+				.numero(30)
+				.build();
+// Asocio el domicilio
+		persona.setDomicilio(domicilio);
+// Guardar el objeto Persona en la base de datos
+		personaRepository.save(persona);
+		// Recuperar el objeto Persona desde la base de datos
+		Persona personaRecuperada = personaRepository.findById(persona.getId()).orElse(null);
+		if (personaRecuperada != null) {
+			System.out.println("Nombre: " + personaRecuperada.getNombre()); System.out.println("Apellido: " +
+					personaRecuperada.getApellido());
+			System.out.println("Edad: " + personaRecuperada.getEdad()); System.out.println("Calle : " +
+					personaRecuperada.getDomicilio().getCalle()); System.out.println("Número :" +
+					personaRecuperada.getDomicilio().getNumero()); }
+	}; }
 
-			// Guardar el objeto Persona en la base de datos
-//        PersonaRepository personaRepository = context.getBean(PersonaRepository.class);
-			personaRepository.save(persona);
 
-			// Recuperar el objeto Persona desde la base de datos
-			Persona personaRecuperada = personaRepository.findById(persona.getId()).orElse(null);
-			if (personaRecuperada != null) {
-				System.out.println("Nombre: " + personaRecuperada.getNombre());
-				System.out.println("Apellido: " + personaRecuperada.getApellido());
-				System.out.println("Edad: " + personaRecuperada.getEdad());
-			}
-
-		};
-
-	}
 
 
 }
--- src/main/java/com/utn/ejercicio1/entidades/Persona.java
+++ src/main/java/com/utn/ejercicio1/entidades/Persona.java
@@ -1,24 +1,23 @@
 package com.utn.ejercicio1.entidades;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Persona implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
     private String nombre;
-
     private String apellido;
     private int edad;
+    // RELACION ONE TO ONE UNIDIRECCIONAL
+// El Cascadeo propaga las operaciones y orphanRemoval asegura que se elimine la relacionada
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true) @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;
 }
